921cfaacfae59c7eeaaeafbb9852c03ad265a264
Dzung Phung <dungdzaj255@gmail.com>
2024-06-24 21:12:59 +0700

    Bullet check collides

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
- 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
 }
