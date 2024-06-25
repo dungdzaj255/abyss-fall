aec6a532342f795776d30632a547960cc8e978a7
Dzung Phung <dungdzaj255@gmail.com>
2024-06-25 16:54:23 +0700

    Update Items

--- Assets/Scripts/Items/DroppedItems/Item.cs
+++ Assets/Scripts/Items/DroppedItems/Item.cs
@@ -14,43 +14,4 @@ public class Item : ScriptableObject
     [SerializeField]
     public int dropChance;
     // Start is called before the first frame update
-
-    static private float DAMAGE = 3f;
-    static private float SMALL_HEALTH = 3f;
-    static private float LARGE_HEALTH = 8f;
-    static private float BULLET_SPEED = 1f;
-
-
-    private void Awake()
-    {
-        Item item = this;
-        this.GetComponent<BoxCollider2D>().isTrigger = true;
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            switch (itemName)
-            {
-                case "Small bandage":
-                    Player.Instance.AddHealth(SMALL_HEALTH);
-                    break;
-                case "Large bandage":
-                    Player.Instance.AddHealth(SMALL_HEALTH);
-                    break;
-                case "Damage":
-                    Weapon.Instance.AddDamage(DAMAGE);
-                    break;
-                case "Bullet":
-                    Weapon.Instance.AddBullet();
-                    break;
-                case "Bullet speed":
-                    Weapon.Instance.AddBulletSpeed(BULLET_SPEED);
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
 }
---
+++ Assets/Scripts/Items/DroppedItems/LootItemPrefab.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LootItemPrefab : MonoBehaviour
+{
+    static private float DAMAGE = 3f;
+    static private float SMALL_HEALTH = 3f;
+    static private float LARGE_HEALTH = 8f;
+    static private float BULLET_SPEED = 1f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            switch (this.name)
+            {
+                case "Small bandage":
+                    Player.Instance.AddHealth(SMALL_HEALTH);
+                    Debug.Log($"Add health +{SMALL_HEALTH}");
+                    break;
+                case "Large bandage":
+                    Player.Instance.AddHealth(LARGE_HEALTH);
+                    Debug.Log($"Add health +{LARGE_HEALTH}");
+                    break;
+                case "Damage":
+                    Weapon.Instance.AddDamage(DAMAGE);
+                    Debug.Log($"Add damage +{DAMAGE}");
+                    break;
+                case "Bullet":
+                    Weapon.Instance.AddBullet();
+                    Debug.Log($"Add bullet amount +1");
+                    break;
+                case "Bullet speed":
+                    Weapon.Instance.AddBulletSpeed(BULLET_SPEED);
+                    Debug.Log($"Add bullet speed +{BULLET_SPEED}");
+                    break;
+                case "Points":
+                    //UIController.Instance.AddPoint();
+                    Debug.Log($"Add bullet speed +{BULLET_SPEED}");
+                    break;
+                default:
+                    break;
+            }
+            Destroy(this.gameObject);
+        }
+        if (!other.transform.CompareTag("Platform"))
+        {
+            Physics2D.IgnoreCollision(this.GetComponent<Collider2D>(), other.transform.GetComponent<Collider2D>());
+        }
+    }
+}
