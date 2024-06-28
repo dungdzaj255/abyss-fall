36df2e733b37986c90b92f4749e7530ae67cdee7
aransumaru <107389800+aransumaru@users.noreply.github.com>
2024-06-28 16:50:09 +0700

    Enemy Flip Direction

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -18,6 +18,7 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField]
     public float damageTouch;
     private Vector3 lastPosition;
+    private bool isMovingBack = false;
 
     // Start is called before the first frame update
     void Start()
@@ -47,6 +48,11 @@ public class EnemyMovement : MonoBehaviour
         if (enemyHealth.GetCurrentHealth() <= 0)
         {
             speed = 0;
+        }
+        if (isMovingBack)
+        {
+            targetPosition = -targetPosition;
+            isMovingBack = false;
         }
         // Move towards the target position
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
@@ -81,6 +87,14 @@ public class EnemyMovement : MonoBehaviour
             //    playerHealth.TakeDamage(damageAmount);
             //}
         }
+        else if (collision.gameObject.tag == "Platform")
+        {
+            FlipDirection();
+        }
 
+    }
+    void FlipDirection()
+    {
+        isMovingBack = true;
     }
 }
