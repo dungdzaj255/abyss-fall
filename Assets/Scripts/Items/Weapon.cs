39792d022b60ab56d02ab7ff58473a2bc85cbc43
Do Hoang Cong Minh <dhcongminh@gmail.com>
2024-07-02 15:18:12 +0700

    add code to use new bullet bar

--- Assets/Scripts/Items/Weapon.cs
+++ Assets/Scripts/Items/Weapon.cs
@@ -8,7 +8,6 @@ public class Weapon : MonoBehaviour
 {
 
     /* bulletbar */
-    [SerializeField] private Bar bulletAmountBar;
     [SerializeField] private BulletCountUI bulletCountUI;
     //======================
     private SpriteRenderer spriteRenderer;
@@ -46,17 +45,6 @@ public class Weapon : MonoBehaviour
 
     // Start is called before the first frame update
     void Start() {
-        /* bulletbar */
-        bulletAmountBar.InitBar(bulletAmount);
-        bulletAmountBar.SetMax(bulletAmount);
-        if (bulletAmount < 10) {
-            bulletAmountBar.SetText("0" + bulletAmount);
-        } else {
-            bulletAmountBar.SetText(bulletAmount + "");
-        }
-
-        bulletCountUI.Init(bulletAmount, currentBulletAmount);
-        //======================
         if (Instance == null)
         {
             Instance = this;
@@ -64,6 +52,10 @@ public class Weapon : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
         BulletPool.InitializePool(bulletAmount);
         currentBulletAmount = bulletAmount;
+        /* new - bulletbar */
+        bulletCountUI.UpdateBar(bulletAmount, currentBulletAmount);
+
+        //======================
         Iddle();
     }
 
@@ -104,14 +96,9 @@ public class Weapon : MonoBehaviour
         bulletAmount++;
         currentBulletAmount++;
         BulletPool.AddBullet();
-        /* bulletbar */
-        bulletAmountBar.SetMax(bulletAmount);
-        bulletAmountBar.SetCurrent(currentBulletAmount);
-        if (bulletAmount < 10) {
-            bulletAmountBar.SetText("0" + currentBulletAmount);
-        } else {
-            bulletAmountBar.SetText(currentBulletAmount + "");
-        }
+        /* new - bulletbar */
+        bulletCountUI.UpdateBar(bulletAmount, currentBulletAmount);
+
         //======================
     }
 
@@ -143,30 +130,24 @@ public class Weapon : MonoBehaviour
                 GetComponentInParent<Rigidbody2D>().AddForce(Vector2.up * recoil, ForceMode2D.Impulse);
             }
 
-            /* bulletbar */
-            bulletAmountBar.SetCurrent(currentBulletAmount);
-            if (bulletAmount < 10) {
-                bulletAmountBar.SetText("0" + currentBulletAmount);
-            } else {
-                bulletAmountBar.SetText(currentBulletAmount + "");
-            }
+            //======================
+            /* new - bulletbar */
+            bulletCountUI.DecreaseByOne(currentBulletAmount);
 
             //======================
+
         }
     }
 
     public void ReloadBullets()
     {
         currentBulletAmount = bulletAmount;
-        /* bulletbar */
-        bulletAmountBar.SetCurrent(currentBulletAmount);
-        if (bulletAmount < 10) {
-            bulletAmountBar.SetText("0" + currentBulletAmount);
-        } else {
-            bulletAmountBar.SetText(currentBulletAmount + "");
-        }
-
         //====================== 
+        //======================
+        /* new - bulletbar */
+        bulletCountUI.UpdateBar(bulletAmount, currentBulletAmount);
+
+        //======================
     }
 
 
